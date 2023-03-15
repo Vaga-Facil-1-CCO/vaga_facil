76818f289805efd4770b3016c84fcaad2c25e756
AdrianoAllexander <adriano.carvalho@sptech.school>
2023-03-15 20:49:05 -0300

    Inserção de dados

--- bancoDeDados/vagaFacil.sql
+++ bancoDeDados/vagaFacil.sql
@@ -120,17 +120,37 @@ datas date,
 tempo time
 );
 
+insert into preferencias values (1,1,'2022-01-20','08:00:30'),
+								(2,3,'2022-10-01','10:30:01'),
+                                (3,2,'2023-01-2','11:55:00'),
+                                (1,1,'2023-01-20','17:40:33'),
+                                (4,1,'2023-02-28','19:06:12'),
+                                (5,1,'2023-03-14','09:40:01'),
+                                (6,1,'2023-03-14','14:29:22');
+                                
+
 create table historico(
 idBairro int,
 dataHota datetime,
-indiceOcupacao int
+indiceOcupacao char(1)
 
 );
 
+
+insert into historico values (1,'2023-02-13 06:04:00','1'),
+							 (2,'2023-02-13 06:04:00','0'),
+                             (3,'2023-02-13 06:04:00','1'),
+							 (4,'2023-02-13 06:04:00','0'),
+							 (5,'2023-02-13 06:04:00','1'),
+							 (6,'2023-02-13 06:04:00','1'),
+							 (7,'2023-02-13 06:04:00','0');
+                             
+
+
 create table dadosEmpresa(
 idEmpresa int primary key auto_increment,
-cnpj char(18) unique,
-endereco varchar(40),
+cnpj char(100) unique,
+endereco varchar(100),
 nomeFantasia varchar(40),
 email varchar(40) unique,
 tel varchar(20),
@@ -138,7 +158,13 @@ dataInicioContrato date,
 dataFinalContrato date
 );
 
-
+insert into dadosEmpresa values (null,'12.528.708/0001-07','Avenida Cruzeiro do Sul,100,Vila Madalena','AgroTech','email@gmail.com','1198765432','2023-10-02','2025-10-02'),
+								(null,'04.128.563/0001-10','Avenida 9 de Julho,134,Liberdade','CafeteriaBrasil','cafe.brasil@gmail.com','1192366432','2024-05-06','2028-09-05'),
+                                (null,'10.338.320/0001-00','Avenida Paulista,422,Sé','BancoBrasil','banco.brasil@gmail.com','1192377432','2024-08-01','2028-05-10'),
+							    (null,'02.217.319/0001-07','Rua Oscar Freire,342,Capão Redondo','utilidades','utilidades@gmail.com','1192323432','2024-09-16','2028-11-06'),
+							    (null,'17.167.396/0001-69','Rua Carlos José de Castilho,462,Saúde','Armarinho','armarinhos@gmail.com','1192377432','2024-10-21','2028-05-10'),
+							    (null,'71.208.516/0001-74','Avenida Onze de Junho,87,São Paulo','comercios LTA','comercios.ltal@gmail.com','119237772','2024-11-30','2028-07-04'),
+							    (null,'	05.878.397/0001-32','Rua Engenheiro Toledo Malta,4566','logistica LTA','logis.tica@gmail.com','11927234674','2024-12-30','2028-01-30');
 
 create table antiFurto(
 idUser int,
@@ -148,15 +174,34 @@ furto char(1)
 
 );
 
+insert into antiFurto values (1,2,'10:00:32','1'),
+						      (2,3,'09:40:44','0'),
+							  (3,2,'11:21:22','0'),
+							  (4,5,'12:30:00','1'),
+							  (5,7,'14:20:02','1'),
+							  (6,4,'15:17:43','1'),
+							  (7,1,'18:03:52','1');
+                              
+
 create table rua(
 idRua int primary key auto_increment,
 nome varchar(40),
 cep char(9)
 );	
 
+insert into rua values (null,'Haddock Lobo','1414001'),
+					   (null,'Avenida Paulista','01310-100'),
+					   (null,'Jardins','01431-010'),
+					   (null,'José neto Julho','12940760'),
+					   (null,'Rua Palanque','08030-110'),
+					   (null,'Avenida Julho Vargas','01308-071'),
+					   (null,'Algusta','01308-071');
+
 create table estado(
 idEstado int primary key auto_increment,
 nome varchar(40),
 sigla char(2)
 );
+
+insert into estado values (null,'São Paulo','SP');
 
